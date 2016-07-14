a1cfb538ae0153a2f77d4ee21f13b066d1ca1455
Guillermo Morales <gjmorale@uc.cl>
2016-07-14 16:52:42 -0300

    Full makeover to rails 5

--- lets/test/controllers/static_pages_controller_test.rb
+++ lets/test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,26 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
-  test "should get landing" do
-    get :landing
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+	def setup
+    @base_title = "LET'S"
+  end
+
+  test "should get root" do
+    get root_url
     assert_response :success
   end
 
-  test "should get about" do
-    get :about
+  test "should get landing" do
+    get static_pages_landing_url
     assert_response :success
+    assert_select "title", "#{@base_title} Welcome"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "#{@base_title} About Us"
   end
 
 end
