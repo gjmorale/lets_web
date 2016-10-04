e7545e0607400dc556c68a7bbf904e484c7d2022
Guillermo Morales <gjmorale@uc.cl>
2016-10-04 12:00:59 -0300

    Account Sign-Up before integration tests

--- lets/app/controllers/accounts_controller.rb
+++ lets/app/controllers/accounts_controller.rb
@@ -1,8 +1,23 @@
 class AccountsController < ApplicationController
   def new
+  	@account = Account.new
   end
 
   def show
   	@account = Account.find(params[:id])
   end
+
+  def create
+  	@account = Account.new(account_params)
+    if @account.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+  	def account_params
+      params.require(:account).permit(:email, :password, :password_confirmation)
+    end
 end
