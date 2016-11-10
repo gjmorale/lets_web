185b7e1f95ab19f1f2a9a41d348dea4a8cca30d4
Guillermo Morales <gjmorale@uc.cl>
2016-11-10 13:46:14 -0300

    Purchases create temp raise 3

--- lets/app/models/offer.rb
+++ lets/app/models/offer.rb
@@ -18,7 +18,7 @@ class Offer < ApplicationRecord
     check! user
     Purchase.generate(user, self)
     event = self.combo.event
-    Admission.get_or_new(user, event)
+    admission = Admission.get_or_new(user, event)
     admission.actual_level = provided_level if Level.compare(admission.actual_level, provided_level)
     admission.save!
   end
